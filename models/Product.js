2e2fe598ba7e0fd7d0cf5a761d62b856a7b6bbe3
faisalantu <faisalantu@gmail.com>
2022-05-06 06:47:59 +0600

    🍐 Product model created

---
+++ models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  userEmail: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
