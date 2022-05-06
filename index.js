b4d7a38d732d662e14fa58eb9af5e8a0378703f4
faisalantu <faisalantu@gmail.com>
2022-05-06 06:46:57 +0600

    📝 added route to index

--- index.js
+++ index.js
@@ -20,16 +20,6 @@ app.get("/", (req, res) => {
 
 //api routes here
 app.use("/api/product", require("./routes/product"));
-// app.use("/api/auth", require("./routes/auth"));
-// app.use("/api/posts", require("./routes/posts"));
-// app.use("/api/likepost", require("./routes/likePost"));
-// app.use("/api/events", require("./routes/events"));
-// app.use("/api/users", require("./routes/users"));
-// app.use("/api/clublist", require("./routes/clubList"));
-// app.use("/api/depertment", require("./routes/depertment"));
-// app.use("/api/role", require("./routes/role"));
-// app.use("/api/setroles", require("./routes/setRoles"));
-// app.use("/api/eventcategory", require("./routes/eventCategory"));
 
 const PORT = process.env.PORT || 5000;
 
