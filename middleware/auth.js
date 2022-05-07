91bc66f86e7935f432933536a4f4a98845f823f1
faisalantu <faisalantu@gmail.com>
2022-05-08 04:08:27 +0600

    🎃 Auth middleware added for jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,19 +2,19 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   //get token from the header
-  const token = req.header("x-auth-token");
+  const token = req.header("authHeader");
 
   //check if not token
   if (!token) {
-    return res.status(401).json({ msg: "no token, auth denied" });
+    return res.status(401).json({ msg: "no token, auth denied 401" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_TOKEN);
 
-    req.user = decoded.user;
+    console.log(decoded);
     next();
   } catch (err) {
-    res.status(401).json({ msg: "token is not valid" });
+    res.status(403).json({ msg: "token is not valid forbiden 403" });
   }
 };
